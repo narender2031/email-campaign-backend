6c189aea4595b333e726f412f86500891789c5fc
Narender Singh <narender2031@gmail.com>
2020-04-11 05:02:14 +0530

    configure passport with google auth

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { googleClientId, googleClientSecret } = require('./config/keys');
 // Define App with express function
 const app = express();
 
+// passport auth
+passport.use(new GoogleStrategy({
+  clientID: googleClientId,
+  clientSecret: googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => { console.log({accessToken: accessToken, refreshToken: refreshToken, profile: profile, done: done}) }));
+
+
 // Route handler
-app.get('/', (req, res) => {
-  res.send({ 'Hi': 'Buddy' });
-})
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+}))
+
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 // Get port dynamically
 const PORT = process.env.PORT || 5000;
