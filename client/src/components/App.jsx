3c051321aef83ee9462176c8d4704a9396ee53bf
Narender Singh <narender2031@gmail.com>
2020-04-14 03:40:03 +0530

    Configure material-css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,19 @@ import * as React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const Landing = () => <h1>Landing</h1>;
-const Header = () => <h1>Header</h1>;
+const Header = () => {
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Emaily</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="#">Login with google</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
 const Dashboard = () => <h1>Dashboard</h1>;
 const SurveyNew = () => <h1>SurveyNew</h1>;
 
