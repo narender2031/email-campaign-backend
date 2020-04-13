0ed603d29865ff476c52d2e53de5f4fff073c6ec
Narender Singh <narender2031@gmail.com>
2020-04-14 01:49:50 +0530

    configure reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import * as React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 
-ReactDom.render(<App />, document.querySelector('#root'));
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
