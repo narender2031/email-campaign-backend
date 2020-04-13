b101da5105484c1bf8130feae87d02c1aaf7c8ee
Narender Singh <narender2031@gmail.com>
2020-04-14 03:17:16 +0530

    configure react-routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+const Landing = () => <h1>Landing</h1>;
+const Header = () => <h1>Header</h1>;
+const Dashboard = () => <h1>Dashboard</h1>;
+const SurveyNew = () => <h1>SurveyNew</h1>;
 
 const App = () => {
   return (
     <div>
-      Hi there
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/survey/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
