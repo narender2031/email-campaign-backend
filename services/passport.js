d2274b8a24890388e430eeaa1b4f907b350d2006
Narender Singh <narender2031@gmail.com>
2020-04-14 01:24:42 +0530

    refactor promises

--- services/passport.js
+++ services/passport.js
@@ -9,32 +9,21 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    })
-})
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id)
+  done(null, user);
+});
 
 passport.use(new GoogleStrategy({
   clientID: googleClientId,
   clientSecret: googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if (existingUser) {
-          // Already existing user
-          done(null, existingUser);
-        } else {
-          // create a new user
-          new User({ googleId: profile.id, name: profile.displayName })
-            .save()
-            .then(user => {
-              done(null, user);
-            })
-        }
-      })
+}, async (accessToken, refreshToken, profile, done) => {
+  const user = await User.findOne({ googleId: profile.id })
+  if (user) {
+    done(null, user);
+  }
+  const newUser = await new User({ googleId: profile.id, name: profile.displayName }).save()
+  done(null, newUser);
 }));
-
